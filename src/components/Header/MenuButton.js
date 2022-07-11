f9d5e027722d844904c971f8f6c1eecaa9e93f9c
Bruno Carvalho <bruno@nuffsaid.com>
2022-07-11 15:22:14 -0300

    refactor: change PathLine color on MenuButton

--- src/components/Header/MenuButton.js
+++ src/components/Header/MenuButton.js
@@ -30,6 +30,10 @@ const PathLine = styled.path`
   stroke-width: 3;
   transition: stroke-dasharray 400ms cubic-bezier(0.4, 0, 0.2, 1),
     stroke-dashoffset 400ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &.white {
+    stroke: #fff;
+  }
 `;
 
 export default function MenuButton({ opened, onClick, size }) {
@@ -41,20 +45,20 @@ export default function MenuButton({ opened, onClick, size }) {
         viewBox="0 0 100 100"
       >
         <PathLine
-          className="pathline-menu"
+          className={size ? 'pathline-menu' : 'pathline-menu white'}
           d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
           strokeDasharray={opened ? '90 207' : '60 207'}
           strokeWidth="3"
           strokeDashoffset={opened ? '-134' : 'initial'}
         />
         <PathLine
-          className="pathline-menu"
+          className={size ? 'pathline-menu' : 'pathline-menu white'}
           d="M 20,50 H 80"
           strokeDasharray={opened ? '1 60' : '60 60'}
           strokeDashoffset={opened ? '-30' : 'initial'}
         />
         <PathLine
-          className="pathline-menu"
+          className={size ? 'pathline-menu' : 'pathline-menu white'}
           d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
           strokeDasharray={opened ? '90 207' : '60 207'}
           strokeDashoffset={opened ? '-134' : 'initial'}
