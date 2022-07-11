f0a0f5e6b381bd32013813c1ccfd7f9977b2ddbc
Bruno Carvalho <bruno@nuffsaid.com>
2022-07-10 21:13:55 -0300

    refactor: index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Layout, { Main, Wrapper } from '../components/Layout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import ProjectCard from '../components/ProjectCard';
 import ProjectsSection from '../components/ProjectsSection';
 import AboutSection from '../components/AboutSection';
 import DepositionsSection from '../components/DepositionsSection';
@@ -19,45 +18,7 @@ export default function Home() {
           <AboutSection />
           <ODSSection />
           <HistorySection />
-          <ProjectsSection>
-            <ProjectCard
-              kind="tenis"
-              title="Tênis"
-              students="80"
-              city="Contagem"
-              schools="E. E. Padre Camargos, E. M. Carlos Drummond de Andrade (CAIC), E. E. Conceição Hilário, E. M. Heitor Villa Lobos
-, E. M. René Chateubriand Domingues"
-              since="2016"
-              locations="Academia Tennis Hall"
-            />
-            <ProjectCard
-              kind="handball"
-              title="Handebol"
-              students="200"
-              city="Betim, Contagem e Ibirité"
-              schools="E. E. Padre Camargos, E. M. do Bairro Petrovale, E. M. Adelina Mesquita Januzzi, E. M. Valério Ferreira Palhares"
-              since="2018"
-              locations="E. E. Padre Camargos, E. M. Adelina Mesquita Januzzi, Ginásio Califórnia, Poliesportivo Petrovale"
-            />
-            <ProjectCard
-              kind="judo"
-              title="Judô"
-              students="120"
-              city="Contagem"
-              schools="E. E. Padre Camargos, E. M. Carlos Drummond de Andrade (CAIC), E. M. Heitor Villa Lobos, E. M. Professora Lígia Magalhães"
-              since="2021"
-              locations="E. E. Padre Camargos, E. M. Carlos Drummond de Andrade (CAIC), E. M. Heitor Villa Lobos, E. M. Professora Lígia Magalhães"
-            />
-            <ProjectCard
-              kind="taekwondo"
-              title="Taekwondo"
-              students="150"
-              city="Betim e Contagem"
-              schools="E. M. do Bairro Petrovale, E. M. Heitor Villa Lobos, E. M. Professora Lígia Magalhães, E. M. Valério Ferreira Palhares"
-              since="2022"
-              locations="Poliesportivo Petrovale, E. M. Heitor Villa Lobos, E. M. Professora Lígia Magalhães"
-            />
-          </ProjectsSection>
+          <ProjectsSection />
           <DepositionsSection />
           <PartnersSection />
           <JoinSection />
