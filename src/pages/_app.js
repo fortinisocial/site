ca05847a8939b18bfbf7994bb5a8821ea8a20362
Bruno Carvalho <bruno@nuffsaid.com>
2022-07-12 19:28:40 -0300

    feat: add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.scss';
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -7,6 +8,19 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-S2RL7YMX85"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="analytics" strategy="afterInteractive">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-S2RL7YMX85');
+            `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
