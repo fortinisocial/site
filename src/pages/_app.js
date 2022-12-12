5647aa9a9739d45d122147a812cdbbc8079725a3
Bruno Carvalho <bruno@solutweb.com.br>
2022-12-12 10:41:52 -0300

    feat: add og image and description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,17 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          property="og:description"
+          content="We are Fortini, a brazilian non-profit civil society organization that promotes access to comprehensive education, sports, and culture for students of public schools in the Metropolitan Region of Belo Horizonte, Minas Gerais, Brazil."
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name" content="Fortini Investimento Social" />
+        <meta
+          property="og:image"
+          content="https://fortini.org/images/fortini.png"
+        />
       </Head>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-S2RL7YMX85"
