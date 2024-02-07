1af9d6803cc38317477aabf4f3c9e4e148e9e18f
Bruno Carvalho <bruno@solutweb.com.br>
2024-02-07 12:37:57 -0300

    fix: donate

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import DonateSection from '../components/DonateSection';
 
-export default function Home() {
+export default function Home({ locale }) {
   return (
     <Layout title={'Fortini Investimento Social - Belo Horizonte'}>
       <Wrapper>
